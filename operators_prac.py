f3700ee8f49f947858a7f3964f3f9049d8f52265
shivam <shivam984861@gmail.com>
2025-11-06 20:35:03 +0530

    new changes

---
+++ operators_prac.py
@@ -0,0 +1,44 @@
+# arthmetic operators 
+# Example : 1
+a = 10
+b = 5
+sum = (a+b)
+diff = (a-b)
+prod = (a*b)
+div = (a/b)
+mod = (a%b)
+exp = (a**b)
+print(sum)
+print(diff)
+print(prod)
+print(div)
+print(mod)
+# we can write the arthmetic operator directly
+a = 2
+b = 4
+print(a+b) 
+print(a-b) 
+print(a*b) 
+print(a/b) 
+print(a%b) 
+
+
+
+# relational operators
+a = 5
+b = 10
+print(a==b)
+# equal to
+print(a!=b)
+# not equal to
+print(a>b)
+# greater than
+print(a<b)
+# less than
+print(a>=b)
+# greater than or equal to
+print(a<=b)
+# less than or equal to
+
+
+
