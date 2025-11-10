c303b681d1f0050803b47b0ad4c998eda7fbdb4b
shivam <shivam984861@gmail.com>
2025-11-10 21:09:13 +0530

    new changes

--- conditional_statement_3.py
+++ conditional_statement_3.py
@@ -70,3 +70,25 @@ print(tax)
 sal = float(input("salary :"))
 tax = sal * (0.1 if sal <= 50000 else 0.2)
 print(tax)
+
+
+
+
+# Example program :
+# HACKERRANK PRACTICE PROBLEM
+"""# (1).Given an integer, n , perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird"""
+
+n = 24
+if(n is "odd" and "odd numbers are weird"):
+    print("weird")
+elif(n is "even" and n in range(2,5)):
+    print("not weird")
+elif(n is "even" and n in range(6,20)):
+    print("weird")
+else:
+    (n > 20 and n is "even")
+    print("not weird")
--- practice2.py
+++ practice2.py
@@ -1,9 +1,9 @@
-name = input("name : ")
-age = int(input("age :"))
-price = float(input("price :"))
-print("my name is",name,"my age is",age)
-"""the above code represents the INPUT RULE in the python 
-where word or any sentence can be written in input statement but the numbers or any other decimal value will be assign as their type"""
+# name = input("name : ")
+# age = int(input("age :"))
+# price = float(input("price :"))
+# print("my name is",name,"my age is",age)
+# """the above code represents the INPUT RULE in the python 
+# where word or any sentence can be written in input statement but the numbers or any other decimal value will be assign as their type"""
 
 
-
+    
