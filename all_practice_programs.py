836f92478e0c40edfddfb8d26a76ce645e7944a4
shivam <shivam984861@gmail.com>
2025-11-13 22:29:24 +0530

    new changes

--- all_practice_programs.py
+++ all_practice_programs.py
@@ -1,60 +1,60 @@
-# #(1).write a program to input 2 numbers and print their sum, difference, product, quotient and remainder
-# number1 = int(input("Enter first number: "))
-# number2 = int(input("Enter second number: "))
+#(1).write a program to input 2 numbers and print their sum, difference, product, quotient and remainder
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
 
-# sum_result = number1 + number2
-# difference_result = number1 - number2
-# product_result = number1 * number2
-# quotient_result = number1 / number2
-# remainder_result = number1 % number2
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+quotient_result = number1 / number2
+remainder_result = number1 % number2
 
-# print("Sum:", sum_result)
-# print("Difference:", difference_result)
-# print("Product:", product_result)
-# print("Quotient:", quotient_result)
-# print("Remainder:", remainder_result)
-
-
-
-# #(2).write a program to input side of a square and print its area and perimeter
-# side1 = float(input("Enter the square side1: "))
-# side2 = int(input("Enter the square side2: "))
-# side3,side4 = int(input("Enter perimeter of the square side3: ")), (input("Enter perimeter of the square side4: "))
-
-# area = side1 * side1
-# print("square side:", side2 ** 2)
-# print("Area of square:", area)
-# print("Perimeter of square:", 4 * side3,4 * side4)
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+print("Remainder:", remainder_result)
 
 
 
-# #(3).write a program to input 2 floating point numbers and print their average
-# num1 = float(input("Enter first number: "))
-# num2 = float(input("Enter second number: "))
-# num3 = int(input("Enter third number: "))
-# num4 = int(input("Enter fourth number: "))
+#(2).write a program to input side of a square and print its area and perimeter
+side1 = float(input("Enter the square side1: "))
+side2 = int(input("Enter the square side2: "))
+side3,side4 = int(input("Enter perimeter of the square side3: ")), (input("Enter perimeter of the square side4: "))
 
-# the_average = (num1 + num2) / 2
-# print("Average:", the_average)
-# print("Average:", (num3 + num4) / 2)
-
-
-
-# #(4).write a program to input 2 int numbers, a and b print true if a is greater than or equal to b otherwise print false
-# var_a  = int(input("Enter first integer (a): "))
-# var_b  = int(input("Enter second integer (b): "))
-# if var_a >= var_b:
-#     print("True")
-# else:
-#     print("False")
+area = side1 * side1
+print("square side:", side2 ** 2)
+print("Area of square:", area)
+print("Perimeter of square:", 4 * side3,4 * side4)
 
 
 
-# # (5).write a program to input users first name and print its length
-# user_1 = input("first name :")
-# firstname = len(user_1)
-# print(firstname)
-# print(len(user_1))   # prints the length of the string
+#(3).write a program to input 2 floating point numbers and print their average
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+
+the_average = (num1 + num2) / 2
+print("Average:", the_average)
+print("Average:", (num3 + num4) / 2)
+
+
+
+#(4).write a program to input 2 int numbers, a and b print true if a is greater than or equal to b otherwise print false
+var_a  = int(input("Enter first integer (a): "))
+var_b  = int(input("Enter second integer (b): "))
+if var_a >= var_b:
+    print("True")
+else:
+    print("False")
+
+
+
+# (5).write a program to input users first name and print its length
+user_1 = input("first name :")
+firstname = len(user_1)
+print(firstname)
+print(len(user_1))   # prints the length of the string
 
 
 
@@ -65,3 +65,4 @@ print("Occurrences of 's':", count)   # prints the number of occurrences of 's' 
 
 str2 = "this is shivam das"
 print(str2.count("s"))   # prints the number of occurrences of 's' in the string
+
