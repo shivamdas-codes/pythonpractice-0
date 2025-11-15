4d22cc1c832646110e0de4e630437fc130662baf
shivam <shivam984861@gmail.com>
2025-11-15 20:29:31 +0530

    new chnages

--- all_practice_programs.py
+++ all_practice_programs.py
@@ -1,84 +1,111 @@
-#(1).write a program to input 2 numbers and print their sum, difference, product, quotient and remainder
-number1 = int(input("Enter first number: "))
-number2 = int(input("Enter second number: "))
+# #(1).write a program to input 2 numbers and print their sum, difference, product, quotient and remainder
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
 
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
-quotient_result = number1 / number2
-remainder_result = number1 % number2
+# sum_result = number1 + number2
+# difference_result = number1 - number2
+# product_result = number1 * number2
+# quotient_result = number1 / number2
+# remainder_result = number1 % number2
 
-print("Sum:", sum_result)
-print("Difference:", difference_result)
-print("Product:", product_result)
-print("Quotient:", quotient_result)
-print("Remainder:", remainder_result)
-
-
-
-#(2).write a program to input side of a square and print its area and perimeter
-side1 = float(input("Enter the square side1: "))
-side2 = int(input("Enter the square side2: "))
-side3,side4 = int(input("Enter perimeter of the square side3: ")), (input("Enter perimeter of the square side4: "))
-
-area = side1 * side1
-print("square side:", side2 ** 2)
-print("Area of square:", area)
-print("Perimeter of square:", 4 * side3,4 * side4)
+# print("Sum:", sum_result)
+# print("Difference:", difference_result)
+# print("Product:", product_result)
+# print("Quotient:", quotient_result)
+# print("Remainder:", remainder_result)
 
 
 
-#(3).write a program to input 2 floating point numbers and print their average
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
-num4 = int(input("Enter fourth number: "))
+# #(2).write a program to input side of a square and print its area and perimeter
+# side1 = float(input("Enter the square side1: "))
+# side2 = int(input("Enter the square side2: "))
+# side3,side4 = int(input("Enter perimeter of the square side3: ")), (input("Enter perimeter of the square side4: "))
 
-the_average = (num1 + num2) / 2
-print("Average:", the_average)
-print("Average:", (num3 + num4) / 2)
-
-
-
-#(4).write a program to input 2 int numbers, a and b print true if a is greater than or equal to b otherwise print false
-var_a  = int(input("Enter first integer (a): "))
-var_b  = int(input("Enter second integer (b): "))
-if var_a >= var_b:
-    print("True")
-else:
-    print("False")
+# area = side1 * side1
+# print("square side:", side2 ** 2)
+# print("Area of square:", area)
+# print("Perimeter of square:", 4 * side3,4 * side4)
 
 
 
-# (5).write a program to input users first name and print its length
-user_1 = input("first name :")
-firstname = len(user_1)
-print(firstname)
-print(len(user_1))   # prints the length of the string
+# #(3).write a program to input 2 floating point numbers and print their average
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# num4 = int(input("Enter fourth number: "))
+
+# the_average = (num1 + num2) / 2
+# print("Average:", the_average)
+# print("Average:", (num3 + num4) / 2)
 
 
 
-# (6).write a program to find the occerrences of S in a string
-str1 = input("enter string :")
-count = str1.count("s")
-print("Occurrences of 's':", count)   # prints the number of occurrences of 's' in the string
+# #(4).write a program to input 2 int numbers, a and b print true if a is greater than or equal to b otherwise print false
+# var_a  = int(input("Enter first integer (a): "))
+# var_b  = int(input("Enter second integer (b): "))
+# if var_a >= var_b:
+#     print("True")
+# else:
+#     print("False")
 
-str2 = "this is shivam das"
-print(str2.count("s"))   # prints the number of occurrences of 's' in the string
+
+
+# # (5).write a program to input users first name and print its length
+# user_1 = input("first name :")
+# firstname = len(user_1)
+# print(firstname)
+# print(len(user_1))   # prints the length of the string
 
 
 
-# (7).write a program to input the grades of a students based on marks
-student_marks = int(input("Enter your marks: "))
-if student_marks >= 90:
-    print("Grade: A")
-elif student_marks >= 80 and student_marks < 90:
-    print("Grade: B")
-elif student_marks >= 70 and student_marks < 80:
-    print("Grade: C") 
-elif student_marks >= 60 and student_marks < 70:
-    print("Grade: D")  
-else:
-    grade = "Fail"
-    print("Grade:", grade)
+# # (6).write a program to find the occerrences of S in a string
+# str1 = input("enter string :")
+# count = str1.count("s")
+# print("Occurrences of 's':", count)   # prints the number of occurrences of 's' in the string
+
+# str2 = "this is shivam das"
+# print(str2.count("s"))   # prints the number of occurrences of 's' in the string
+
+
+
+# # (7).write a program to input the grades of a students based on marks
+# student_marks = int(input("Enter your marks: "))
+# if student_marks >= 90:
+#     print("Grade: A")
+# elif student_marks >= 80 and student_marks < 90:
+#     print("Grade: B")
+# elif student_marks >= 70 and student_marks < 80:
+#     print("Grade: C") 
+# elif student_marks >= 60 and student_marks < 70:
+#     print("Grade: D")  
+# else:
+#     grade = "Fail"
+#     print("Grade:", grade)
     
+
+
+# # (8).write a program to check if a number is entered by the user is even or odd
+# num = int (input("Enter a number: "))
+# rem = num % 2
+# if rem == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+
+
+# # (9).write a program to find the greatest among 3 numbers entered by the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if num1 >= num2 and num1 >= num3:
+    greatest = num1
+    print("The greatest number is:", num1)
+elif num2 >= num1 and num2 >= num3:
+    greatest = num2
+    print("The greatest number is:", num2)  
+elif num3 >= num1 and num3 >= num2:
+    greatest = num3
+    print("The greatest number is:", num3)
+else:
+    print("All numbers are equal.")
--- conditional_statement_3.py
+++ conditional_statement_3.py
@@ -1,158 +1,169 @@
-# this code indicates about the conditional statements in python, which is mainly used in decision making
-# example code 1
-light = input("light colour =")
-if(light == "red"):
-    print("stop")
-elif(light == "yellow"):
-    print("get ready")
-elif(light == "green"):
-    print("go")
-else:
-    print("if light is broken")
+# # this code indicates about the conditional statements in python, which is mainly used in decision making
+# # example code 1
+# light = input("light colour =")
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("get ready")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("if light is broken")
 
 
-# example code 2
-marks = input("marks :")
-if(marks >= "90"):
-    print("grade A")
-elif(marks >= "80" and marks < "90"):
-    print("grade B")
-elif(marks >= "70" and marks < "80"):
-    print("grade C")
-else:
-    print("fail")
-# in the above code the input statement always return the string not the integer value
-# in the code the "and" operator is used to combine two conditions together
+# # example code 2
+# marks = input("marks :")
+# if(marks >= "90"):
+#     print("grade A")
+# elif(marks >= "80" and marks < "90"):
+#     print("grade B")
+# elif(marks >= "70" and marks < "80"):
+#     print("grade C")
+# else:
+#     print("fail")
+# # in the above code the input statement always return the string not the integer value
+# # in the code the "and" operator is used to combine two conditions together
 
 
-# example code 3
-a = input("a :")
-g = input("f/m :")
-if((a == 2 or a == 1) and g == "m"):
-    print("fee is 200")
-elif(a == 3 or a == 5 or g == "f"):
-    print("fee is 300")
-elif(a == 4 and g == "m"):
-    print("fee is 400")
-else:
-    print("no fee")
-
-
-
-# this codes comes under ternary operator also known as the single line comment in python 
-# we can write in two ways 
-food = input("food :")
-order = "yes" if food == "cake" else "no"
-print(order)
-
-# first by assigning the variable value in it 
-
-food = input("food :")
-print("sweet") if(food == "cake" or food == "icecream") else print("not sweet")
-# second by directly using print statement in it
+# # example code 3
+# a = input("a :")
+# g = input("f/m :")
+# if((a == 2 or a == 1) and g == "m"):
+#     print("fee is 200")
+# elif(a == 3 or a == 5 or g == "f"):
+#     print("fee is 300")
+# elif(a == 4 and g == "m"):
+#     print("fee is 400")
+# else:
+#     print("no fee")
 
 
 
+# # this codes comes under ternary operator also known as the single line comment in python 
+# # we can write in two ways 
+# food = input("food :")
+# order = "yes" if food == "cake" else "no"
+# print(order)
 
-# this code also part of the ternary operator but in different way, also known as the "clever if" statement
-a = int(input("age :"))
-vote = ("yes","no") [a >= 18]
-print(vote)
+# # first by assigning the variable value in it 
+
+# food = input("food :")
+# print("sweet") if(food == "cake" or food == "icecream") else print("not sweet")
+# # second by directly using print statement in it
 
 
 
 
-# we can write a clever if statement in 2 ways...
-sal = float(input("salary :"))
-tax = sal *(0.1,0.2) [sal > 50000]
-print(tax)
-
-sal = float(input("salary :"))
-tax = sal * (0.1 if sal <= 50000 else 0.2)
-print(tax)
+# # this code also part of the ternary operator but in different way, also known as the "clever if" statement
+# a = int(input("age :"))
+# vote = ("yes","no") [a >= 18]
+# print(vote)
 
 
 
 
-# Example program :
-# HACKERRANK PRACTICE PROBLEM
-"""# (1).Given an integer, n , perform the following conditional actions:
-If n is odd, print Weird
-If n is even and in the inclusive range of  to , print Not Weird
-If n is even and in the inclusive range of  to , print Weird
-If n is even and greater than , print Not Weird"""
+# # we can write a clever if statement in 2 ways...
+# sal = float(input("salary :"))
+# tax = sal *(0.1,0.2) [sal > 50000]
+# print(tax)
 
-n = 24
-if(n is "odd" and "odd numbers are weird"):
-    print("weird")
-elif(n is "even" and n in range(2,5)):
-    print("not weird")
-elif(n is "even" and n in range(6,20)):
-    print("weird")
-else:
-    (n > 20 and n is "even")
-    print("not weird")
+# sal = float(input("salary :"))
+# tax = sal * (0.1 if sal <= 50000 else 0.2)
+# print(tax)
 
 
 
-# part- 2
-age = int(input("enter your age: "))
-if (age < 18):
-    print("you are a minor")
-elif (age >= 18 and age < 65):
-    print("you are an adult")
-else:
-    print("you are a senior citizen")
+
+# # Example program :
+# # HACKERRANK PRACTICE PROBLEM
+# """# (1).Given an integer, n , perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of  to , print Not Weird
+# If n is even and in the inclusive range of  to , print Weird
+# If n is even and greater than , print Not Weird"""
+
+# n = 24
+# if(n is "odd" and "odd numbers are weird"):
+#     print("weird")
+# elif(n is "even" and n in range(2,5)):
+#     print("not weird")
+# elif(n is "even" and n in range(6,20)):
+#     print("weird")
+# else:
+#     (n > 20 and n is "even")
+#     print("not weird")
 
 
 
-num = 100
-if(num >= 2):
-    print("true")
-if(num < 10):
-    print("true")   
-if(num == 5):
-    print("true")
-elif(num != 5):
-    print("false")
-else:
-    print("none")
+# # part- 2
+# age = int(input("enter your age: "))
+# if (age < 18):
+#     print("you are a minor")
+# elif (age >= 18 and age < 65):
+#     print("you are an adult")
+# else:
+#     print("you are a senior citizen")
 
 
 
-voter_age = int(input("Enter your age: "))
-if voter_age >= 18:
-    print("You are eligible to vote.")
-# if voter_age < 18:
-#     print("You are not eligible to vote.")    
-# elif voter_age == 18:
-#     print("You have just become eligible to vote.")
-else:
-    print("Invalid age entered.")    
+# num = 100
+# if(num >= 2):
+#     print("true")
+# if(num < 10):
+#     print("true")   
+# if(num == 5):
+#     print("true")
+# elif(num != 5):
+#     print("false")
+# else:
+#     print("none")
+
+
+
+# voter_age = int(input("Enter your age: "))
+# if voter_age >= 18:
+#     print("You are eligible to vote.")
+# # if voter_age < 18:
+# #     print("You are not eligible to vote.")    
+# # elif voter_age == 18:
+# #     print("You have just become eligible to vote.")
+# else:
+#     print("Invalid age entered.")    
 
    
 
-# NESTED IF STATEMENT
-# example 1
-age = int(input("Enter your age: "))
-if age >= 18:
-    print("You are eligible to vote.")
-    citizenship = input("Are you a citizen? (yes/no): ")
-    if citizenship.lower() == "yes":
-        print("You can register to vote.")
+# # NESTED IF STATEMENT
+# # example 1
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to vote.")
+#     citizenship = input("Are you a citizen? (yes/no): ")
+#     if citizenship.lower() == "yes":
+#         print("You can register to vote.")
+#     else:
+#         print("You must be a citizen to register to vote.")
+
+
+# # example 2
+# marks = int(input("Enter your marks: "))
+# if marks >= 90 and marks <= 100:
+#     print("Grade: A")
+#     if marks >= 95 and marks < 100:
+#         print("Excellent performance!")
+#         if marks == 100:
+#             print("Perfect score!")
+# elif marks >= 80 and marks < 90:
+#     print("Grade: B")
+# else:
+#     print("Good job!")
+
+
+# # example 3
+num = 75
+if num % 5 == 0:
+    if num % 2 == 0:
+        print("java")
     else:
-        print("You must be a citizen to register to vote.")
-
-
-# example 2
-marks = int(input("Enter your marks: "))
-if marks >= 90 and marks <= 100:
-    print("Grade: A")
-    if marks >= 95 and marks < 100:
-        print("Excellent performance!")
-        if marks == 100:
-            print("Perfect score!")
-elif marks >= 80 and marks < 90:
-    print("Grade: B")
+        print("python")
 else:
-    print("Good job!")
+    print("c++")
